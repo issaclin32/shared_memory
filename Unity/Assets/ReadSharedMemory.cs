bfea94fe7341ea4fa6c4ed0ebf471925eefbdf07
issaclin32 <issaclin32@gmail.com>
2019-05-07 14:42:24 +0800

    Add files via upload

---
+++ Unity/Assets/ReadSharedMemory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class ReadSharedMemory : MonoBehaviour {
+    public uint SHARED_MEMORY_SIZE = 1024;
+    public string TAG_NAME = "Local\\Test";
+    SharedMem map;
+
+    void Start () {
+        Debug.Log("[ReadSharedMemory] initializing objects...");
+        // only for .NET 4.0 and above
+        //MemoryMappedFile mmf = MemoryMappedFile.CreateNew(TAG_NAME, SHARED_MEMORY_SIZE);
+
+        // for all versions of .NET
+        map = new SharedMem(TAG_NAME, true, SHARED_MEMORY_SIZE);
+    }
+	
+	void Update () {
+        IntPtr root = map.Root;
+
+        if (root == null) {
+            Debug.Log("Cannot locate shared memory. Check if the Python side is turned on.");
+        }
+        else
+        {
+            string content = Marshal.PtrToStringAnsi(root);
+            Debug.Log("content(of shared mem): " + content);
+        }
+    }
+}
---
+++ Unity/Assets/SharedMem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class SharedMem : IDisposable
+{
+    // Here we're using enums because they're safer than constants
+
+    enum FileProtection : uint      // constants from winnt.h
+    {
+        ReadOnly = 2,
+        ReadWrite = 4
+    }
+
+    enum FileRights : uint          // constants from WinBASE.h
+    {
+        Read = 4,
+        Write = 2,
+        ReadWrite = Read + Write
+    }
+
+    static readonly IntPtr NoFileHandle = new IntPtr(-1);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr CreateFileMapping(IntPtr hFile,
+                                            int lpAttributes,
+                                            FileProtection flProtect,
+                                            uint dwMaximumSizeHigh,
+                                            uint dwMaximumSizeLow,
+                                            string lpName);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr OpenFileMapping(FileRights dwDesiredAccess,
+                                          bool bInheritHandle,
+                                          string lpName);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr MapViewOfFile(IntPtr hFileMappingObject,
+                                        FileRights dwDesiredAccess,
+                                        uint dwFileOffsetHigh,
+                                        uint dwFileOffsetLow,
+                                        uint dwNumberOfBytesToMap);
+    [DllImport("Kernel32.dll")]
+    static extern bool UnmapViewOfFile(IntPtr map);
+
+    [DllImport("kernel32.dll")]
+    static extern int CloseHandle(IntPtr hObject);
+
+    IntPtr fileHandle, fileMap;
+
+    public IntPtr Root { get { return fileMap; } }
+
+    public SharedMem(string name, bool existing, uint sizeInBytes)
+    {
+        if (existing)
+            fileHandle = OpenFileMapping(FileRights.ReadWrite, false, name);
+        else
+            fileHandle = CreateFileMapping(NoFileHandle, 0,
+                                            FileProtection.ReadWrite,
+                                            0, sizeInBytes, name);
+        if (fileHandle == IntPtr.Zero)
+            throw new Exception
+              ("Open/create error: " + Marshal.GetLastWin32Error());
+
+        // Obtain a read/write map for the entire file
+        fileMap = MapViewOfFile(fileHandle, FileRights.ReadWrite, 0, 0, 0);
+
+        if (fileMap == IntPtr.Zero)
+            throw new Exception
+              ("MapViewOfFile error: " + Marshal.GetLastWin32Error());
+    }
+
+    public void Dispose()
+    {
+        if (fileMap != IntPtr.Zero) UnmapViewOfFile(fileMap);
+        if (fileHandle != IntPtr.Zero) CloseHandle(fileHandle);
+        fileMap = fileHandle = IntPtr.Zero;
+    }
+}
